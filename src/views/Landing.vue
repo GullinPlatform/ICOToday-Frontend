2420305b850681c49a6f7af3ed22593e53b9cd38
Joseph Kitonga <kitongajoseph@gmail.com>
2017-09-05 05:19:13 -0400

    added static today's top icos

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,25 +1,76 @@
 <template>
     <div>
-      <!-- Off-Canvas Wrapper-->
-         <div class="offcanvas-wrapper">
-           <!-- Page Content-->
-           <!-- Main Slider-->
-           <section class="hero-slider" style="background-image: url(img/rocket-image.png);">
+
+           <section class="hero-slider" style="background-image: url('../../../static/img/rocket-image.png');">
                <div class="item">
                  <div class="container padding-top-3x">
                    <div class="row justify-content-center align-items-center">
                      <div class="col-lg-5 col-md-6 padding-bottom-2x text-md-left text-center">
-                       <div class="from-bottom"><img class="d-inline-block w-200 mb-4" src="img/rocket-image.png" alt="Converse">
-                         <div class="h2 text-body text-normal mb-2 pt-1">Chuck Taylor All Star II</div>
-                         <div class="h2 text-body text-normal mb-4 pb-1">for only <span class="text-bold">$59.99</span></div>
-                       </div><a class="btn btn-primary scale-up delay-1" href="shop-single.html">Shop Now</a>
+
                      </div>
-                     <div class="col-md-6 padding-bottom-2x mb-3"><img class="d-block mx-auto" src="img/rocket-image.png" alt="Chuck Taylor All Star II"></div>
+
                    </div>
                  </div>
                </div>
            </section>
-         </div>
+
+         <!-- Featured Products Carousel-->
+      <section class="container padding-top-3x padding-bottom-3x">
+        <h3 class="text-left mb-30">Today's Top ICOs</h3>
+        <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;576&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:4}} }">
+          <!-- Product-->
+          <div class="grid-item">
+            <div class="product-card">
+              <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="shop-single.html"><img src="../../static/img/shop/products/09.jpg" alt="Product"></a>
+              <h3 class="product-title"><a href="shop-single.html">Rocket Dog</a></h3>
+              <h4 class="product-price">
+                <del>$44.95</del>$34.99
+              </h4>
+              <div class="product-buttons">
+                <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
+                <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+          <!-- Product-->
+          <div class="grid-item">
+            <div class="product-card">
+                <div class="rating-stars"><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star"></i>
+                </div><a class="product-thumb" href="shop-single.html"><img src="../../static/img/shop/products/03.jpg" alt="Product"></a>
+              <h3 class="product-title"><a href="shop-single.html">Oakley Kickback</a></h3>
+              <h4 class="product-price">$155.00</h4>
+              <div class="product-buttons">
+                <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
+                <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+          <!-- Product-->
+          <div class="grid-item">
+            <div class="product-card"><a class="product-thumb" href="shop-single.html"><img src="../../static/img/shop/products/12.jpg" alt="Product"></a>
+              <h3 class="product-title"><a href="shop-single.html">Vented Straw Fedora</a></h3>
+              <h4 class="product-price">$49.50</h4>
+              <div class="product-buttons">
+                <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
+                <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+          <!-- Product-->
+          <div class="grid-item">
+            <div class="product-card">
+                <div class="rating-stars"><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i>
+                </div><a class="product-thumb" href="shop-single.html"><img src="../../static/img/shop/products/11.jpg" alt="Product"></a>
+              <h3 class="product-title"><a href="shop-single.html">Top-Sider Fathom</a></h3>
+              <h4 class="product-price">$90.00</h4>
+              <div class="product-buttons">
+                <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
+                <button class="btn btn-outline-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
         <!-- ======== @Region: #content ======== -->
         <div id="content" class="pt-5 pb-6 bg-white pos-relative pos-zindex-10">
