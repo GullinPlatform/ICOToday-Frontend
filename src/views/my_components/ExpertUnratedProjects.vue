f9293fa7b64375bdfab37a2d3687f812e3033ae1
C0deZ Lee <c0dezlee@gmail.com>
2017-10-24 00:21:47 +0100

    Add ExpertUnratedProjects back

---
+++ src/views/my_components/ExpertUnratedProjects.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="col-md-8">
+    <h6 class="text-muted text-normal text-uppercase">
+      Unrated Projects
+    </h6>
+    <hr class="mb-3 mt-2">
+    <project-list :loaded="loaded" :projects="unrated_projects"></project-list>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import ProjectList from 'src/views/components/ProjectList'
+
+  export default {
+    name: 'AnalystUnratedProjects',
+    data () {
+      return {
+        loaded: false,
+      }
+    },
+    head: {
+      title: {
+        inner: 'ICOToday',
+        complement: 'Unrated Projects'
+      }
+    },
+    components: {
+      ProjectList
+    },
+    methods: {
+      loadProjects () {
+        this.loaded = false
+        // My Marked Projects
+        this.$store.dispatch('listUnratedProjects')
+          .then(() => {
+            this.loaded = true
+          })
+      },
+    },
+    computed: {
+      ...mapGetters({
+        me: 'self',
+        self_type: 'self_type',
+        is_verified: 'is_verified',
+        login_status: 'login_status',
+        unrated_projects: 'unrated_projects'
+      })
+    },
+    beforeCreate () {
+      // redirect not verified user
+      if (!this.$store.getters.is_verified) {
+        this.$router.push({name: 'me_need_verify'})
+      }
+    },
+    beforeMount () {
+      // redirect non ico analyst user
+      if (this.self_type !== 2) {
+        this.$router.push({name: 'landing'})
+      }
+
+      this.loadProjects()
+    },
+  }
+</script>
